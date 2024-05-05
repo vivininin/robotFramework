ee9fba40e5de3fd404a5062a1994dc65de50e696
vivininin <vivininin@protonmail.com>
2024-05-05 11:23:39 +0300

    Update settings

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/ViewModels/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshak2.Models.Entities;
+
+namespace Mooshak2.Models.ViewModels
+{
+    public class StudentViewModel
+    {
+        public Enrollment ID { get; set; }
+        public Course Title { get; set; }
+        public ApplicationUser Name { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
