e04c504bd8b1e08fded91df43dedb66d90269049
vivininin <vivininin@protonmail.com>
2024-05-06 11:23:39 +0300

    add new components

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.ViewModels
+{
+    public class CoursesViewModel
+    {
+        
+    }
+}
