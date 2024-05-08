1ae362c0c5832b16acb7b2e7089212ebd3893a26
vivininin <vivininin@protonmail.com>
2024-05-08 11:23:39 +0300

    Improve database indexing

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/Teachment.cs
@@ -0,0 +1,12 @@
+﻿namespace Mooshak2.Models.Entities
+{
+    public class Teachment
+    {
+        public int TeachmentID { get; set; }
+        public int CourseID { get; set; }
+
+
+        public virtual Course Course { get; set; }
+        public virtual ApplicationUser UserID { get; set; }
+    }
+}
