abe720192977729897e3d59a9496fa2917bf14c3
vivininin <vivininin@protonmail.com>
2024-05-09 11:23:39 +0300

    update test cases

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/SubmissionFile.cs
@@ -0,0 +1,12 @@
+﻿namespace Mooshak2.Models.Entities
+{
+    public class SubmissionFile
+    {
+        public int SubmissionFileID { get; set; }
+        public int FileID { get; set; }
+        public int SubmissionID { get; set; }
+
+        public virtual File File { get; set; }
+        public virtual Submission Submission { get; set; }
+    }
+}
