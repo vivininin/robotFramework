614305e7563b8a2e7b0fa3a7be61761ec691d7fc
vivininin <vivininin@protonmail.com>
2024-05-09 11:23:39 +0300

    Fixed bug in user login

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/Submission.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Mooshak2.Models.Entities
+{
+    public class Submission
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int AssignmentMilestoneID { get; set; }
+
+        public virtual ApplicationUser UserID { get; set; }
+        public virtual ICollection<SubmissionFile> SubmissionFiles { get; set; }
+
+    }
+}
