a313dd5ba9df2aefa1f9fd0c44d9ff0016d83c71
vivininin <vivininin@protonmail.com>
2024-05-10 11:23:39 +0300

    Updated assets

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/File.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Mooshak2.Models.Entities
+{
+    public class File
+    {
+        public int FileID { get; set; }
+        public string Title { get; set; }
+        public string PathToFile { get; set; }
+
+        public virtual ICollection<SubmissionFile> SubmissionFiles { get; set; }
+    }
+}
