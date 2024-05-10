a6a783534247c0ab9e24d41ce62203dce0405872
vivininin <vivininin@protonmail.com>
2024-05-10 11:23:39 +0300

    improve session management

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/IdentityModels.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Mooshak2.Models.Entities
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() : base() { }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Teachment> Teachments { get; set; }
+        public virtual ICollection<Submission> Submissions { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+   
+}
