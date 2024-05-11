dce200b0df856213bfa923a65fb0b3bc166b3ffa
vivininin <vivininin@protonmail.com>
2024-05-11 11:23:39 +0300

    fixed layout issues

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Models/Entities/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Mooshak2.Models.Entities
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual ApplicationUser UserID { get; set; }
+    }
+}
