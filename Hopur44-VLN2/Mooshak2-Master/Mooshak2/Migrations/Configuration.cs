b6017244aecf83919573298bdee3195361f54d31
vivininin <vivininin@protonmail.com>
2024-05-13 11:23:39 +0300

    add tests

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+using Mooshak2.DAL;
+using Mooshak2.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Mooshak2.Migrations
+{
+
+
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Mooshak2.DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
