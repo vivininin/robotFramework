7d9fd8d40ea40d19066c256438e27556279b82c7
vivininin <vivininin@protonmail.com>
2024-05-23 11:23:39 +0300

    update translations

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/DAL/SubmissionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.DAL
+{
+    public class SubmissionService
+    {
+        //private readonly ApplicationDbContext _dbContext = new ApplicationDbContext();
+    }
+}
