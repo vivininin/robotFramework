a533ac526bba9d84904884a2f50cc44f6de81bdd
vivininin <vivininin@protonmail.com>
2024-05-26 11:23:39 +0300

    add new util

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Mooshak2.DAL;
+using Mooshak2.Models.ViewModels;
+using Mooshak2.Exceptions;
+
+namespace Mooshak2.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class StudentController : Controller
+    {
+        private UserService _userService = new UserService();
+        private CoursesService _coursesService = new CoursesService();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            var curruser = _userService.GetUserById(User.Identity.GetUserId());
+            var corses = _coursesService.GetCoursesForStudent(curruser);
+            return View(corses);
+        }
+    }
+}
