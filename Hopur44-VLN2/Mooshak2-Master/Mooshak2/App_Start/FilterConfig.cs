ea6bbaa9e7ac79ef03f61ce2b1b0198de619b91e
vivininin <vivininin@protonmail.com>
2024-06-04 11:23:39 +0300

    add new components

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using Mooshak2.Handlers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshak2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+			//filters.Add(new HandleErrorAttribute());
+			filters.Add(new CustomHandleErrorAttribute());
+		}
+    }
+}
