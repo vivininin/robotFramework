b5b69f1c16712d018895e5254582bfaab32a8b40
vivininin <vivininin@protonmail.com>
2024-06-07 11:23:39 +0300

    add new routes

---
+++ Hopur44-VLN2/Mooshak2-Master/Mooshak2/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Principal;
+using System.Web.Security;
+using Microsoft.Owin;
+using Mooshak2.DAL;
+using Owin;
+using Mooshak2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartupAttribute(typeof(Mooshak2.Startup))]
+namespace Mooshak2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            ConfigureAuth(app);
+        }
+    }
+}
